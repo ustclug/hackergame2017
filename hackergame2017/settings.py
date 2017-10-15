cce0506e2e7fd1e523a5dea478aeaead4c343f49
hejiyan <hejiyan@mail.ustc.edu.cn>
2017-10-15 19:58:22 +0800

    modify start time to pause the game

--- hackergame2017/settings.py
+++ hackergame2017/settings.py
@@ -20,7 +20,8 @@ SITE = {
     'login': cas + 'login?service=%s' % service,
     'logout': cas + 'logout',
     'validate': cas + 'serviceValidate?service=%s&ticket=%%s' % service,
-    'starttime': 1507953600,  # 2017-10-14T12:00:00+08:00
+    #'starttime': 1507953600,  # 2017-10-14T12:00:00+08:00
+    'starttime': 1508108400,  # 2017-10-16T07:00:00+08:00
     'endtime': 1508558400,  # 2017-10-21T12:00:00+08:00
 }
 
